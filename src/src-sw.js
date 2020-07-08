61d0107034de73aec5efc6c6493b1bb2684bb294
Edward J <edjeong99@gmail.com>
2020-07-08 17:31:49 -0500

    work on cache update-install workbox-window

---
+++ src/src-sw.js
@@ -0,0 +1,23 @@
+import { precacheAndRoute } from 'workbox-precaching';
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+workbox.routing.registerRoute(
+  /https:\/\/api\.exchangeratesapi\.io\/latest/,
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'currencies',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 10 * 60, // 10 minutes
+      }),
+    ],
+  })
+);
+
+addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    skipWaiting();
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 const path = require("path");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 
 module.exports = env => {
   const mode = env.mode ? env.mode : "production";
@@ -16,12 +17,17 @@ module.exports = env => {
       path: path.resolve(__dirname, "dist")
     },
     plugins: [
-      new webpack.HashedModuleIdsPlugin(),
-      new HtmlWebpackPlugin({
-        template: "./index.html",
-        minify: { collapseWhitespace: true, removeComments: true },
-        inject: false
-      })
+    new webpack.HashedModuleIdsPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      minify: { collapseWhitespace: true, removeComments: true },
+      inject: false
+    })
+    , new WorkboxWebpackPlugin.InjectManifest({
+      swSrc: "./src/src-sw.js",
+      swDest: "sw.js"
+    })
+
     ],
     devtool: "source-map"
   };
